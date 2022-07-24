af62988e274ccddba399326b484806701d820526
andreu323 <andreu33.ok@gmail.com>
2022-07-24 19:55:49 +0300

    changed tab connection

--- vizualno_ronunru.js
+++ vizualno_ronunru.js
@@ -1,49 +1,28 @@
 
-tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     
     if(changeInfo.status == "complete"){
         
         if(tab.url.includes("https://spworlds.ru") &&  tab.url.includes("lawsuits")  &&  !tab.url.includes("lawsuits/")){
-            tabs.query(
-            {
-             lastFocusedWindow: true,
-             active: true
-            },
-            function (tabs)
-            {
-                tabs.executeScript({
-                    file: 'sude_plus.js'
-                  }); 
-            });
-        
+            console.log(browser.tabs);
+            browser.tabs.executeScript({
+                file: 'sude_plus.js'
+                
+              }); 
             
         }
         else if(tab.url.includes("https://spworlds.ru") &&  tab.url.includes("lawsuits/") && !tab.url.includes("/new") ){
-            tabs.query(
-                {
-                 lastFocusedWindow: true,
-                 active: true
-                },
-                function (tabs)
-                {
-                    tabs.executeScript({
+            browser.tabs.executeScript({
                         file: 'delo.js'
                       }); 
-            });
+            
         }
         if( tab.url.includes("https://spworlds.ru") &&  tab.url.includes("about")){
 
-            tabs.query(
-                {
-                lastFocusedWindow: true,
-                active: true
-                },
-                function (tabs)
-                {
-                    tabs.executeScript({
+            browser.tabs.executeScript({
                         file: 'about.js'
                     }); 
-                });
+            
         }
     }
     
