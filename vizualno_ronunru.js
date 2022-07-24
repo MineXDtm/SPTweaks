94674a527f94924054ec3de1af9b7002c15fca8a
lodo4nik <65357022+lodo4nik@users.noreply.github.com>
2022-07-24 18:27:20 +0300

    mama

--- vizualno_ronunru.js
+++ vizualno_ronunru.js
@@ -1,16 +1,16 @@
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if(changeInfo.status == "complete"){
         
         if(tab.url.includes("https://spworlds.ru") &&  tab.url.includes("lawsuits")  &&  !tab.url.includes("lawsuits/")){
-            chrome.tabs.query(
+            tabs.query(
             {
              lastFocusedWindow: true,
              active: true
             },
             function (tabs)
             {
-                chrome.tabs.executeScript({
+                tabs.executeScript({
                     file: 'sude_plus.js'
                   }); 
             });
@@ -18,28 +18,28 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
             
         }
         else if(tab.url.includes("https://spworlds.ru") &&  tab.url.includes("lawsuits/") && !tab.url.includes("/new") ){
-            chrome.tabs.query(
+            tabs.query(
                 {
                  lastFocusedWindow: true,
                  active: true
                 },
                 function (tabs)
                 {
-                    chrome.tabs.executeScript({
+                    tabs.executeScript({
                         file: 'delo.js'
                       }); 
             });
         }
         if( tab.url.includes("https://spworlds.ru") &&  tab.url.includes("about")){
 
-            chrome.tabs.query(
+            tabs.query(
                 {
                 lastFocusedWindow: true,
                 active: true
                 },
                 function (tabs)
                 {
-                    chrome.tabs.executeScript({
+                    tabs.executeScript({
                         file: 'about.js'
                     }); 
                 });
